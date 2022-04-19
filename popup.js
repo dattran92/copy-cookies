c6aadbcca18332a5cd949e5e1f86613e6ed6d112
Dat Tran <tranthanhdat.it@gmail.com>
2022-04-19 08:56:10 +0700

    copy to clipboard

--- popup.js
+++ popup.js
@@ -1,8 +1,14 @@
-const form = document.getElementById("control-row");
-const go = document.getElementById("go");
+const form = document.getElementById("copy-to-other");
+const accrossDomainSubmit = document.getElementById("copy-to-other-submit");
 const fromDomainInput = document.getElementById("from_domain");
 const toDomainInput = document.getElementById("to_domain");
 const keyPatternInput = document.getElementById("key_pattern");
+
+const clipboardForm = document.getElementById("copy-to-clipboard");
+const clipboardSubmit = document.getElementById("clipboard-submit");
+const clipboardFromDomainInput = document.getElementById("clipboard_from_domain");
+const clipboardKeyInput = document.getElementById("clipboard_key");
+
 const message = document.getElementById("message");
 
 function getStorageSyncData(keys) {
@@ -37,14 +43,18 @@ function setStorageSyncData(data) {
 
 // The async IIFE is necessary because Chrome <89 does not support top level await.
 (async function initPopupWindow() {
-  const savedValues = await getStorageSyncData(['from_domain', 'to_domain', 'key_pattern'])
+  const savedValues = await getStorageSyncData(['from_domain', 'to_domain', 'key_pattern', 'clipboard_from_domain', 'clipboard_key'])
   fromDomainInput.value = savedValues.from_domain || '';
   toDomainInput.value = savedValues.to_domain || '';
   keyPatternInput.value = savedValues.key_pattern || '';
+  clipboardFromDomainInput.value = savedValues.clipboard_from_domain || '';
+  clipboardKeyInput.value = savedValues.clipboard_key || '';
   fromDomainInput.focus();
 })();
 
 form.addEventListener("submit", handleFormSubmit);
+
+clipboardForm.addEventListener("submit", handleClipboardSubmit);
 
 async function handleFormSubmit(event) {
   event.preventDefault();
@@ -59,6 +69,24 @@ async function handleFormSubmit(event) {
   })
   const message = await copyCookiesValues(fromDomain, toDomain, pattern)
   setMessage(message);
+}
+
+async function handleClipboardSubmit(event) {
+  event.preventDefault();
+  clearMessage();
+  const fromDomain = clipboardFromDomainInput.value || '';
+  const key = clipboardKeyInput.value || '';
+  if (fromDomain && key) {
+    const cookie = await chrome.cookies.get({ name: key, url: fromDomain });
+    const value = cookie?.value;
+    copyTextToClipboard(value);
+    setMessage(`Copied ${value} to clipboard`);
+  }
+
+  await setStorageSyncData({
+    clipboard_from_domain: fromDomain,
+    clipboard_key: key,
+  })
 }
 
 async function copyCookiesValues(fromDomain, toDomain, pattern) {
@@ -112,4 +140,30 @@ function setMessage(str) {
 function clearMessage() {
   message.hidden = true;
   message.textContent = "";
+}
+
+function copyTextToClipboard(text) {
+  //Create a textbox field where we can insert text to.
+  var copyFrom = document.createElement("textarea");
+
+  //Set the text content to be the text you wished to copy.
+  copyFrom.textContent = text;
+
+  //Append the textbox field into the body as a child.
+  //"execCommand()" only works when there exists selected text, and the text is inside
+  //document.body (meaning the text is part of a valid rendered HTML element).
+  document.body.appendChild(copyFrom);
+
+  //Select all the text!
+  copyFrom.select();
+
+  //Execute command
+  document.execCommand('copy');
+
+  //(Optional) De-select the text using blur().
+  copyFrom.blur();
+
+  //Remove the textbox field from the document.body, so no other JavaScript nor
+  //other elements can get access to this.
+  document.body.removeChild(copyFrom);
 }
